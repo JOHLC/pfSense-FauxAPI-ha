5bcdfa5031c60fee8f79fbaa5222052987fda9f7
Joel <63735432+JOHLC@users.noreply.github.com>
2021-04-24 12:14:57 -0400

    Update __init__.py

--- custom_components/pfsense_fauxapi/__init__.py
+++ custom_components/pfsense_fauxapi/__init__.py
@@ -1,1 +1,9 @@
 """pfSense FauxAPI for Home Assistant"""
+
+DOMAIN = "pfsense_fauxapi"
+
+def setup(hass, config):
+    import PfsenseFauxapi
+    #from funkapi import FunkAPI
+    # Return boolean to indicate that initialization was successful.
+    return True
